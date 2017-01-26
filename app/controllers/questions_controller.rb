44650e00ee7b6960ee564907ab3827012ba74c45
NYCSwan <megan.swanby@gmail.com>
2017-01-26 12:33:18 -0500

    Add routes for questions controller including show, new/create and private methods. Add views for index/show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,33 @@
 class QuestionsController < ApplicationController
 
   def index
-    questions = Question.all
+    @questions = Question.all
   end
 
   def show
-
+  	@question = Question.find(params[:id])
   end
 
+  def new
+  	question = Question.new
+  end
+
+  def create
+  	question = build_content
+
+  	if question.save
+  		redirect_to '/'
+  	else 
+  		redirect_to '/'
+  	end
+  end
+
+  private
+  def build_content
+  	Question.new(question_params)
+  end
+
+  def question_params
+  	params.require(:question).permit(:body, :user_id) 
+  end
 end
